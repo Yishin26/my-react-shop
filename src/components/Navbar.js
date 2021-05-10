4ae2633121643be4be0ae7e546e54b994ab822b3
Louise <yishinlin@outlook.com>
2021-05-10 22:43:30 +0800

    + sass loader

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 
-import './Navbar.css';
+import '../style/Navbar.scss';
 const Navbar = () => (
     <div className='navbar'>
-        導航列
+        <div className='title'>導航列</div>
     </div>
 );
 export default Navbar;
