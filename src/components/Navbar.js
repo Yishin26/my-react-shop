c2da627485902d66caadda982ce89ea9d7bbb055
Louise <yishinlin@outlook.com>
2021-05-11 21:38:09 +0800

    + Navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,9 @@
 import '../style/Navbar.scss';
 const Navbar = () => (
     <div className='navbar'>
-        <div className='title'>導航列</div>
+        <div className='title'>我的店</div>
+        <input className='filter' type="text" placeholder='搜尋篩選....' />
+        <div className='user-name'>UserName</div>
     </div>
 );
 export default Navbar;
